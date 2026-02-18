c16061aed872b7202a16e221377d2034ca1c1ea8
R433N <raa105@sfu.ca>
2026-02-17 22:55:07 -0800

    added seeding setup

---
+++ seeds/cities.js
@@ -0,0 +1,125 @@
+const cities = [
+
+/* ==================== BRITISH COLUMBIA ==================== */
+{ name: "Vancouver", province: "BC" },
+{ name: "Burnaby", province: "BC" },
+{ name: "Surrey", province: "BC" },
+{ name: "Richmond", province: "BC" },
+{ name: "Coquitlam", province: "BC" },
+{ name: "Kelowna", province: "BC" },
+{ name: "Victoria", province: "BC" },
+{ name: "Langley", province: "BC" },
+{ name: "Abbotsford", province: "BC" },
+{ name: "Nanaimo", province: "BC" },
+
+/* ==================== ALBERTA ==================== */
+{ name: "Calgary", province: "AB" },
+{ name: "Edmonton", province: "AB" },
+{ name: "Red Deer", province: "AB" },
+{ name: "Lethbridge", province: "AB" },
+{ name: "Medicine Hat", province: "AB" },
+{ name: "Grande Prairie", province: "AB" },
+{ name: "Airdrie", province: "AB" },
+{ name: "Spruce Grove", province: "AB" },
+{ name: "St. Albert", province: "AB" },
+{ name: "Fort McMurray", province: "AB" },
+
+/* ==================== SASKATCHEWAN ==================== */
+{ name: "Saskatoon", province: "SK" },
+{ name: "Regina", province: "SK" },
+{ name: "Prince Albert", province: "SK" },
+{ name: "Moose Jaw", province: "SK" },
+{ name: "Yorkton", province: "SK" },
+{ name: "Estevan", province: "SK" },
+{ name: "Weyburn", province: "SK" },
+{ name: "Swift Current", province: "SK" },
+{ name: "North Battleford", province: "SK" },
+{ name: "Lloydminster", province: "SK" },
+
+/* ==================== MANITOBA ==================== */
+{ name: "Winnipeg", province: "MB" },
+{ name: "Brandon", province: "MB" },
+{ name: "Steinbach", province: "MB" },
+{ name: "Thompson", province: "MB" },
+{ name: "Portage la Prairie", province: "MB" },
+{ name: "Selkirk", province: "MB" },
+{ name: "Morden", province: "MB" },
+{ name: "Winkler", province: "MB" },
+{ name: "Dauphin", province: "MB" },
+{ name: "The Pas", province: "MB" },
+
+/* ==================== ONTARIO ==================== */
+{ name: "Toronto", province: "ON" },
+{ name: "Ottawa", province: "ON" },
+{ name: "Hamilton", province: "ON" },
+{ name: "London", province: "ON" },
+{ name: "Mississauga", province: "ON" },
+{ name: "Brampton", province: "ON" },
+{ name: "Waterloo", province: "ON" },
+{ name: "Windsor", province: "ON" },
+{ name: "Kingston", province: "ON" },
+{ name: "Sudbury", province: "ON" },
+
+/* ==================== QUEBEC ==================== */
+{ name: "Montreal", province: "QC" },
+{ name: "Quebec City", province: "QC" },
+{ name: "Laval", province: "QC" },
+{ name: "Gatineau", province: "QC" },
+{ name: "Sherbrooke", province: "QC" },
+{ name: "Trois-Rivières", province: "QC" },
+{ name: "Saguenay", province: "QC" },
+{ name: "Drummondville", province: "QC" },
+{ name: "Longueuil", province: "QC" },
+{ name: "Saint-Jean-sur-Richelieu", province: "QC" },
+
+/* ==================== NEW BRUNSWICK ==================== */
+{ name: "Fredericton", province: "NB" },
+{ name: "Moncton", province: "NB" },
+{ name: "Saint John", province: "NB" },
+{ name: "Dieppe", province: "NB" },
+{ name: "Miramichi", province: "NB" },
+{ name: "Edmundston", province: "NB" },
+{ name: "Bathurst", province: "NB" },
+{ name: "Campbellton", province: "NB" },
+{ name: "Riverview", province: "NB" },
+{ name: "Quispamsis", province: "NB" },
+
+/* ==================== NOVA SCOTIA ==================== */
+{ name: "Halifax", province: "NS" },
+{ name: "Sydney", province: "NS" },
+{ name: "Dartmouth", province: "NS" },
+{ name: "Truro", province: "NS" },
+{ name: "New Glasgow", province: "NS" },
+{ name: "Glace Bay", province: "NS" },
+{ name: "Kentville", province: "NS" },
+{ name: "Yarmouth", province: "NS" },
+{ name: "Bridgewater", province: "NS" },
+{ name: "Amherst", province: "NS" },
+
+/* ==================== PRINCE EDWARD ISLAND ==================== */
+{ name: "Charlottetown", province: "PE" },
+{ name: "Summerside", province: "PE" },
+{ name: "Stratford", province: "PE" },
+{ name: "Cornwall", province: "PE" },
+{ name: "Montague", province: "PE" },
+{ name: "Kensington", province: "PE" },
+{ name: "Souris", province: "PE" },
+{ name: "Alberton", province: "PE" },
+{ name: "Georgetown", province: "PE" },
+{ name: "Tignish", province: "PE" },
+
+/* ==================== NEWFOUNDLAND & LABRADOR ==================== */
+{ name: "St. John's", province: "NL" },
+{ name: "Mount Pearl", province: "NL" },
+{ name: "Corner Brook", province: "NL" },
+{ name: "Conception Bay South", province: "NL" },
+{ name: "Grand Falls-Windsor", province: "NL" },
+{ name: "Gander", province: "NL" },
+{ name: "Labrador City", province: "NL" },
+{ name: "Happy Valley-Goose Bay", province: "NL" },
+{ name: "Stephenville", province: "NL" },
+{ name: "Paradise", province: "NL" }
+
+];
+
+module.exports = cities;
