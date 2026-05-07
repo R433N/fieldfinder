40f53b2fc68eb0ad235c5b9d176e4b12dcf56f63
R433N <raa105@sfu.ca>
2026-05-06 19:31:34 -0700

    restructuring field routes

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const field = require('./models/field');
 const Review = require('./models/review');  
 const { constants } = require('os');
 
+const fields = require('./routes/fields');
+
 mongoose.connect('mongodb://localhost:27017/fieldfinder')
 
 const db = mongoose.connection;
@@ -29,18 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-const validateField = (req, res, next) => {
-       //if(!req.body.field) throw new ExpressError('Invalid Field Data', 400)
 
-   const { error } = fieldSchema.validate(req.body);
-   if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-   }
-   else{
-        next();
-   }
-}
 
 const validateReview = (req, res, next) => {
 
@@ -54,51 +45,13 @@ const validateReview = (req, res, next) => {
     }
 }
 
+app.use('/fields', fields);
+
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/fields', catchAsync(async (req, res) => {
-    const fields = await Field.find({});
-    res.render('fields/index', { fields });
-}));
 
-app.get('/fields/new', (req, res) => {
-    res.render('fields/new');
-})
-
-app.post('/fields', validateField, catchAsync(async (req, res) => {
-
-    const field = new Field(req.body.field);
-    await field.save();
-    res.redirect(`/fields/${field._id}`);
-}))
-
-app.get('/fields/:id', catchAsync(async (req, res) => {
-     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    throw new ExpressError('Invalid Field ID', 400);
-  }
-    const field = await Field.findById(req.params.id).populate('reviews');
-    console.log(field);
-    res.render('fields/show', {field});
-}))
-
-app.get('/fields/:id/edit', catchAsync(async (req, res) => {
-    const field = await Field.findById(req.params.id);
-    res.render('fields/edit', {field});
-}))
-
-app.put('/fields/:id', validateField, catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const field = await Field.findByIdAndUpdate(id, { ...req.body.field });
-    res.redirect(`/fields/${field._id}`);
-}))
-
-app.delete("/fields/:id", catchAsync(async (req,res) => {
-    const{id} = req.params;
-    await Field.findByIdAndDelete(id);
-    res.redirect('/fields');
-}))
 
 app.post('/fields/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const field = await Field.findById(req.params.id);
