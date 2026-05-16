9f3b571fa7a59599d179c5b62ec417d0441c307b
R433N <raa105@sfu.ca>
2026-05-16 16:44:22 -0700

    adding permissions, cleaned up code

--- middleware.js
+++ middleware.js
@@ -1,3 +1,6 @@
+const {fieldSchema, reviewSchema} = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -12,4 +15,40 @@ module.exports.storeReturnTo = (req, res, next) => {
         res.locals.returnTo = req.session.returnTo;
     }
     next();
+}
+
+
+module.exports.validateField = (req, res, next) => {
+       //if(!req.body.field) throw new ExpressError('Invalid Field Data', 400)
+
+   const { error } = fieldSchema.validate(req.body);
+   if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+   }
+   else{
+        next();
+   }
+}
+
+module.exports.isAuthor = async( req, res, next) => {
+    const {id} = req.params;
+    const field = await Field.findById(id)
+    if(!field.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to edit this field')
+        return res.redirect(`/fields/${id}`);
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+
+    const {error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else{
+        next();
+    }
 }
