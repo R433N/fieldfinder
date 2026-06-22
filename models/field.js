9dc0bef0cfc967d79cf04872263e6575b2a38e1d
R433N <raa105@sfu.ca>
2026-06-21 23:27:53 -0700

    adding thumbnail virtual property for editting page

--- models/field.js
+++ models/field.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() { 
+    return this.url.replace('/upload', '/upload/w_200');
+})
 const fieldSchema = new Schema({
     title:  String ,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     location:  String ,
     fieldType:  String,
     phone: { type: String },
