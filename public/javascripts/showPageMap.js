fd05852780baaa41e53b0cf08046b1a77e732f3a
R433N <raa105@sfu.ca>
2026-07-23 17:32:01 -0700

    fixed json bug, fixed coordinates bug, fixed display issues, current

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -4,11 +4,16 @@ const coordinates = field.geometry.coordinates;
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: coordinates,
     zoom: 10
 });
-
 new mapboxgl.Marker()
     .setLngLat(coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset:25})
+        .setHTML(
+            `<h3>${field.title}</h3><p>${field.location}</p>`
+        )
+    )
     .addTo(map);
